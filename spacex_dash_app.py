1d478ba531a6d8f230c6c6615e702a3d9b874eee
amroca79 <amroca79@gmail.com>
2022-02-19 10:39:23 -0800

    Edit the callback fucntion. Added local variables.

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -8,14 +8,10 @@ import plotly.express as px
 
 spacex_df = pd.read_csv(r'C:\Users\Deadend\DS_Projects\IBM-Applied-Data-Science-Capstone\spacex_launch_dash.csv')
 
-success_rate = spacex_df.groupby(['LaunchSite'])['Class'].value_counts().reset_index(name='Count')
-
 max_payload = spacex_df['PayloadMass'].max()
 min_payload = spacex_df['PayloadMass'].min()
 
-mark_values={0:'0kg',1000:'1000kg',2000:'2000kg',3000:'3000kg',4000:'4000kg',5000:'5000kg',6000:'6000kg',7000:'7000kg',8000:'8000kg',9000:'9000kg',10000:'10000kg'}
-
-payload_rate = spacex_df[['LaunchSite', 'Class', 'PayloadMass', 'Block_Version']].sort_values(by=['LaunchSite', 'PayloadMass', 'Block_Version'], ascending=True).reset_index(drop=True)
+mark_values={0:'0kg',1000:'1000kg',2000:'2000kg',3000:'3000kg',4000:'4000kg',5000:'5000kg',6000:'6000kg',7000:'7000kg',8000:'8000kg',9000:'9000kg',10000:'10000kg',11000:'11000kg',12000:'12000kg',13000:'13000kg',14000:'14000kg',15000:'15000kg',16000:'16000kg'}
 
 app = dash.Dash(__name__)
 
@@ -29,7 +25,7 @@ app.layout = html.Div([html.Div(children=[html.H1('SpaceX Launch Records Dashboa
                        html.Br(),
                        
                        html.P("Payload range (Kg):"),
-                       html.Div([dcc.RangeSlider(id='payload_slider',min=0, max=10000, step=1000,value=[min_payload , max_payload],marks=mark_values)],style={'width':'98%','position':'relative','left':'1%'}),
+                       html.Div([dcc.RangeSlider(id='payload_slider',min=0, max=16000, step=1000,value=[min_payload , max_payload],marks=mark_values)],style={'width':'98%','position':'relative','left':'1%'}),
                        html.Br(),
                        
                        html.Div([dcc.Graph(id='success-payload-scatter-chart',figure={})])
@@ -40,9 +36,10 @@ app.layout = html.Div([html.Div(children=[html.H1('SpaceX Launch Records Dashboa
 
 def get_pie_chart(site_dropdown):
     
-    df_copy = success_rate
-    all_sites = df_copy[df_copy['Class']==1]
-    launch_site = df_copy[df_copy['LaunchSite']==str(site_dropdown)]
+    df_copy = spacex_df
+    success_rate = df_copy.groupby(['LaunchSite'])['Class'].value_counts().reset_index(name='Count')
+    all_sites = success_rate[success_rate['Class']==1]
+    launch_site = success_rate[success_rate['LaunchSite']==str(site_dropdown)]
     
     if site_dropdown == 'All':
         fig = px.pie(all_sites, values='Count', names='LaunchSite')
@@ -58,15 +55,19 @@ def get_pie_chart(site_dropdown):
 
 def get_scatter(payload_slider, site_dropdown):
     
-    df_copy = payload_rate
-    all_sites = df_copy[df_copy['PayloadMass'].between(int(payload_slider[0]), int(payload_slider[1]))]
-    launch_site = df_copy[(df_copy['LaunchSite'] == str(site_dropdown)) & (df_copy['PayloadMass'].between(int(payload_slider[0]), int(payload_slider[1])))]
+    df_copy = spacex_df
+
+    cluster_count = df_copy.groupby(['Block_Version','Class'])['PayloadMass'].value_counts().reset_index(name='Count')
+    cluster_count2 = df_copy.groupby(['LaunchSite','Class','Block_Version'])['PayloadMass'].value_counts().reset_index(name='Count')
+    
+    all_sites = cluster_count[cluster_count['PayloadMass'].between(int(payload_slider[0]), int(payload_slider[1]))]
+    launch_site = cluster_count2[(cluster_count2['LaunchSite'] == str(site_dropdown)) & (df_copy['PayloadMass'].between(int(payload_slider[0]), int(payload_slider[1])))]
     
     if site_dropdown == 'All':
-        fig = px.scatter(all_sites, x='PayloadMass', y='Class', color='Block_Version')
+        fig = px.scatter(all_sites, x='PayloadMass', y='Class', color='Block_Version', hover_data=['Block_Version','PayloadMass','Count'])
         return(fig)
     else:
-        fig = px.scatter(launch_site, x='PayloadMass', y='Class', color='Block_Version')
+        fig = px.scatter(launch_site, x='PayloadMass', y='Class', color='Block_Version', hover_data=['Block_Version','PayloadMass','Count'])
         return(fig)
     
 
